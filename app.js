f3a9a3f3d882eb3e8a1225c6b5e6c2bef77ba492
MehdiHenna <mehdihennaoui@gmail.com>
2015-12-04 12:33:04 +0100

    exercices diver sur javascript

---
+++ app.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+
+var foo =  3;
+
+
+
+function fonction (arg) {
+
+	return arg * foo;
+
+
+}
+
+
+
+
+function fuckction () {
+
+	return foo =  5;
+
+}
+
+
+alert(foo);
+
+
+
+});
+
+
+
+
+
